c85eabb920c8b47b86ed9b389d23ab84d42a2d61
Umut Yurdugül <umtyrdql@gmail.com>
2025-11-12 19:21:35 +0300

    First commit.
    Opening and closing tickets

---
+++ bot.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+
+token = "TOKENİ BURAYA YAZIN"
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot çalışıyor: {bot.user}")
+
+
+@bot.command()
+async def ticket(ctx):
+    guild = ctx.guild
+    user = ctx.author
+    channel_name = f"ticket-{user.name}"
+
+    existing_channel = discord.utils.get(guild.text_channels, name=channel_name)
+    if existing_channel:
+        await ctx.send(f"{user.mention}, zaten açık bir ticketin var {existing_channel.mention} !")
+        return
+
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        user: discord.PermissionOverwrite(read_messages=True, send_messages=True),
+        guild.me: discord.PermissionOverwrite(read_messages=True)
+    }
+
+    ticket_channel = await guild.create_text_channel(channel_name, overwrites=overwrites)
+    await ticket_channel.send(f"{user.mention}, ticket açtın! Birazdan yetkili gelir.")
+    await ctx.send(f"{user.mention}, ticket kanalın oluşturuldu: {ticket_channel.mention}")
+
+@bot.command()
+async def close(ctx):
+    if ctx.channel.name.startswith("ticket-"):
+        await ctx.send("Ticket kapatılıyor...")
+        await ctx.channel.delete()
+    else:
+        await ctx.send("Bu komut sadece ticket kanallarında kullanılabilir.")
+
+bot.run(token)
